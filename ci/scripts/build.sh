c1135fea39cb62423e508180ae728970602b9a1a
kalyan <kalyanappari002@gmail.com>
2022-12-07 17:28:54 +0530

    script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,8 +2,6 @@
 #!/bin/sh
 set -e
 
-repository=$(pwd)/distribution-repository
-
 cd git-repo
-	./mvnw clean deploy -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
+	./mvnw clean install
 cd ..
